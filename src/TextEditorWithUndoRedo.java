72e1fb40435a0bbd8fc4ccede2d5987762b6ad1d
CompanyProfile <alanood94895@gmail.com>
2026-04-26 13:52:03 +0400

    Task9: TextEditorWithUndoRedo

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 public class TextEditorWithUndoRedo {
     public static void main(String[] args){
 
-        Stack <String> undoStack = new Stack<>();
+        Stack <String> undoStack = new Stack<>();  // stack for undo Actions
 
     }
 }
