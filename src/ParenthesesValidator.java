78bd92af86c366af8f0643f31f99159992e316d6
CompanyProfile <alanood94895@gmail.com>
2026-04-26 10:44:09 +0400

    Task5: ParenthesesValidator

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -1,6 +1,9 @@
+import java.util.Stack;
+
 public class ParenthesesValidator {
     public static void main (String[] args){
-
+        // Create the Stack
+        Stack<String> webPageURLs = new Stack<>();
     }
 
 }
