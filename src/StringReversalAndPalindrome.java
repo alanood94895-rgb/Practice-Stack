3b7ac69fd34af633bf5d264b6235cfbae7f4d404
CompanyProfile <alanood94895@gmail.com>
2026-04-26 11:40:34 +0400

    Task6:StringReversalAndPalindrome

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -3,7 +3,14 @@ import java.util.Stack;
 public class StringReversalAndPalindrome {
     public static void main (String[] args){
         // Create the Stack
-        Stack<String> ParenthesesValidator = new Stack<>();
+        Stack<String> String = new Stack<>();
+
+        String.push("Java");
+        String.push("");
+        String.push("");
+        String.push("");
+        String.push("");
+        String.push("");
 
     }
 }
