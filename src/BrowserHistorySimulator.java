12dd7f7094f26f67665be7cb4f0912127ab0e5eb
CompanyProfile <alanood94895@gmail.com>
2026-04-26 10:13:22 +0400

    Task4: BrowserHistorySimulator

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -1,5 +1,8 @@
+import java.util.Stack;
+
 public class BrowserHistorySimulator {
     public static void main (String[] args){
-
+        // Create the Stack
+        Stack<String> employeeAction = new Stack<>();
     }
 }
