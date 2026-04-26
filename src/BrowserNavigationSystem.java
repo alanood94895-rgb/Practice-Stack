03cf680fc1284e3c0981ba52356c3e78ffdb9721
CompanyProfile <alanood94895@gmail.com>
2026-04-26 16:01:58 +0400

    Task12:BrowserNavigationSystem

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -6,6 +6,7 @@ public class BrowserNavigationSystem {
     }
     // Back and Forward stacks
     static Stack<String> backStack = new Stack<>();
+    static Stack<String> forwardStack = new Stack<>();
 
 
 }
