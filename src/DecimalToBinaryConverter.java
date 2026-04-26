37950c3eacb68b42a60bea859b295cf6189b3336
CompanyProfile <alanood94895@gmail.com>
2026-04-26 12:05:46 +0400

    Task7:DecimalToBinaryConverter

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -2,6 +2,9 @@ public class DecimalToBinaryConverter {
     public static void main (String[] args){
 
         // 6 test decimal numbers
+        int[] numbers = {0, 5, 10, 13, 32, 255};
+        System.out.println("Decimal number");
+
 
 
     }
