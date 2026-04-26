50e5659e3665835566ce45b20d20d9b438544cde
CompanyProfile <alanood94895@gmail.com>
2026-04-26 16:07:14 +0400

    Task12:BrowserNavigationSystem

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -1,69 +1,74 @@
 import java.util.Stack;
 
 public class BrowserNavigationSystem {
+
+    // Back and Forward stacks
+    static Stack<String> backStack = new Stack<>();
+    static Stack<String> forwardStack = new Stack<>();
+
+    // Current page
+    static String currentPage = "Home";
+
+    // Limit for back history
+    static final int MAX_HISTORY = 10;
+
     public static void main(String[] args) {
 
+        showCurrentPage();
 
-        // Back and Forward stacks
-        static Stack<String> backStack = new Stack<>();
-        static Stack<String> forwardStack = new Stack<>();
+        // Simulated user actions (12+ actions)
+        visitPage("Google");
+        visitPage("YouTube");
+        visitPage("Facebook");
+        goBack();
+        goBack();
+        goForward();
+        visitPage("Twitter");
+        goBack();
+        visitPage("LinkedIn");
+        goBack();
+        goForward();
+        goForward(); // edge case
 
-        // Current page
-        static String currentPage = "Home";
-
-        // Limit for back history
-        static final int MAX_HISTORY = 10;
-
-        public static void main (String[]args){
-            showCurrentPage();
-
-            // Simulated user actions
-            visitPage("Google");
-            visitPage("YouTube");
-            visitPage("Facebook");
-            goBack();
-            goBack();
-            goForward();
-            visitPage("Twitter");
-            goBack();
-            visitPage("LinkedIn");
-            goBack();
-            goForward();
-            goForward();
-
-            showSessionHistory();
-        }
-        // Visit new page
-        public static void visitPage(String url) {
-// Save current page to back stack
-            backStack.push(currentPage);
-            // Maintain max history
-            if (backStack.size() > MAX_HISTORY) {
-                backStack.remove(0); // remove oldest
-            }
-            currentPage = url;
-
-            // Clear forward history
-            forwardStack.clear();
-
-            System.out.println("Visited: " + url);
-            display();
-        }
-        // Go back
-        public static void goBack() {
-
-            if (backStack.isEmpty()) {
-                System.out.println("Cannot go back");
-                return;
-            }
-            forwardStack.push(currentPage);
-
-            currentPage = backStack.pop();
-
-            System.out.println("Went Back");
-            display();
-        }
+        showSessionHistory();
     }
+
+    // Visit new page
+    public static void visitPage(String url) {
+
+        // Save current page to back stack
+        backStack.push(currentPage);
+
+        // Maintain max history
+        if (backStack.size() > MAX_HISTORY) {
+            backStack.remove(0); // remove oldest
+        }
+
+        currentPage = url;
+
+        // Clear forward history
+        forwardStack.clear();
+
+        System.out.println("Visited: " + url);
+        display();
+    }
+
+    // Go back
+    public static void goBack() {
+
+        if (backStack.isEmpty()) {
+            System.out.println("Cannot go back");
+            return;
+        }
+
+        forwardStack.push(currentPage);
+
+        currentPage = backStack.pop();
+
+        System.out.println("Went Back");
+        display();
+    }
+
     // Go forward
     public static void goForward() {
 
@@ -79,11 +84,13 @@ public class BrowserNavigationSystem {
         System.out.println("Went Forward");
         display();
     }
+
     // Show current page
     public static void showCurrentPage() {
         System.out.println("Current Page: " + currentPage);
         System.out.println("----------------------");
     }
+
     // Display stacks + current page
     public static void display() {
         System.out.println("Current Page: " + currentPage);
@@ -91,6 +98,7 @@ public class BrowserNavigationSystem {
         System.out.println("Forward Stack: " + forwardStack);
         System.out.println("----------------------");
     }
+
     // Bonus: show all pages visited in session
     public static void showSessionHistory() {
         System.out.println("Session History:");
