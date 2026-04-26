28320ed181e10f310f3eb4c99151a0f80d2561ad
CompanyProfile <alanood94895@gmail.com>
2026-04-26 16:03:49 +0400

    Task12:BrowserNavigationSystem

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -1,20 +1,22 @@
 import java.util.Stack;
 
 public class BrowserNavigationSystem {
-    public static void main (String[] args){
-
-    }
-    // Back and Forward stacks
-    static Stack<String> backStack = new Stack<>();
-    static Stack<String> forwardStack = new Stack<>();
-
-    // Current page
-    static String currentPage = "Home";
-
-    // Limit for back history
-    static final int MAX_HISTORY = 10;
-
     public static void main(String[] args) {
 
 
+        // Back and Forward stacks
+        static Stack<String> backStack = new Stack<>();
+        static Stack<String> forwardStack = new Stack<>();
+
+        // Current page
+        static String currentPage = "Home";
+
+        // Limit for back history
+        static final int MAX_HISTORY = 10;
+
+        public static void main (String[]args){
+            showCurrentPage();
+
+        }
     }
+}
