543d9a7a024d85ae00a8c5c4bd750fe94875c2f7
CompanyProfile <alanood94895@gmail.com>
2026-04-26 09:21:45 +0400

    Task1:EmployeeStackDemo

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -3,12 +3,11 @@ import java.util.Stack;
 public class EmployeeStackDemo {
     public static void main (String[] args){
         Stack <String> employeeAction = new Stack<>();
-        employeeAction.push("IT");
-        employeeAction.push("HR");
-        employeeAction.push("Manager");
-        employeeAction.push("");
-        employeeAction.push();
-
+        employeeAction.push("A logged in");
+        employeeAction.push("B updated client records");
+        employeeAction.push("C generated report");
+        employeeAction.push("D approved leave request");
+        employeeAction.push("E logged out");
 
 
     }
