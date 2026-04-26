b3bfa69fcf68602ebf819451539608ba4c0d33fd
CompanyProfile <alanood94895@gmail.com>
2026-04-26 14:07:16 +0400

    Task10: HtmlTagValidator

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -1,2 +1,16 @@
 public class HtmlTagValidator {
-}
+    public static void main(String[] args) {
+
+        // 8 test HTML/XML strings
+        String[] tests = {
+                "<html><body><p>Hello</p></body></html>",
+                "<div><b>Bold</b></div>",
+                "<div><b>Wrong</i></div>",
+                "<html><head></head><body><h1>Title</h1></body></html>",
+                "<p><i>Text</p></i>",
+                "<br/>",
+                "",
+                "<div class='box'><p>Test</p></div>"
+        };
+
+    }
