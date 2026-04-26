a39f6ee77a3a4d226b872ecdbdbf59e1aef71e0f
CompanyProfile <alanood94895@gmail.com>
2026-04-26 09:32:33 +0400

    Task3:EmployeeStackSearch

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -4,13 +4,11 @@ public class EmployeeStackSearch {
     public static void main (String[] args){
         Stack <String> employeeAction = new Stack<>();
 
-        employeeAction.push();
-        employeeAction.push();
-        employeeAction.push();
-        employeeAction.push();
-        employeeAction.push();
-        employeeAction.push();
-
+        employeeAction.push("A logged in");
+        employeeAction.push("B updated client records");
+        employeeAction.push("C generated report");
+        employeeAction.push("D approved leave request");
+        employeeAction.push("E logged out");
 
     }
 }
