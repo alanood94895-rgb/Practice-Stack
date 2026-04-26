cbc7e7d2b6024de094d81ac091e211f65a9e601f
CompanyProfile <alanood94895@gmail.com>
2026-04-26 09:26:13 +0400

    Task1:EmployeeStackDemo

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -4,15 +4,15 @@ public class EmployeeStackDemo {                     // Create the main class
     public static void main (String[] args){
         // Create the Stack
         Stack <String> employeeAction = new Stack<>();
-        employeeAction.push("A logged in");
-        employeeAction.push("B updated client records");
-        employeeAction.push("C generated report");
-        employeeAction.push("D approved leave request");
-        employeeAction.push("E logged out");
+        employeeAction.push("employee Ali logged in");
+        employeeAction.push("employee Noof updated client records");
+        employeeAction.push("employee Noor generated report");
+        employeeAction.push("employee Hoor approved leave request");
+        employeeAction.push("employee Alanoud logged out");
 
         System.out.println(employeeAction);
 
-        employeeAction.push("z change Passwords");
+        employeeAction.push("employee Said change Passwords");
         System.out.println("Updated Employee Actions in Stack:");
         System.out.println(employeeAction);
     }
