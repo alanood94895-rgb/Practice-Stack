b057b25226c35a534d3fdcb35e71ff855fa6ed60
CompanyProfile <alanood94895@gmail.com>
2026-04-26 12:27:58 +0400

    Task8: PostfixEvaluator

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -1,13 +1,13 @@
 public class PostfixEvaluator {
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
         //Create 6 String element
-        String s1="1 2 +";
-        String s2="3 4 -";
-        String s3="5 7 *";
-        String s4="10 1 %";
-        String s5="2 9 /";
-        String s6="7 8 + *";
+        String s1 = "1 2 +";
+        String s2 = "3 4 -";
+        String s3 = "5 7 *";
+        String s4 = "10 1 %";
+        String s5 = "2 9 /";
+        String s6 = "7 8 + *";
 
         for (String expr : expressions) {
             System.out.println("Expression: " + expr);
@@ -17,5 +17,8 @@ public class PostfixEvaluator {
             System.out.println("Result: " + result);
 
         }
+    }
+    String[] tokens = expression.split(" ");
 
+        for (String token : tokens) {
     }
