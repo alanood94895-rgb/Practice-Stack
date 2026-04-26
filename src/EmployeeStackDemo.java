7990f7c3a22df9bcaa82f8549d1b351cb37a3e95
CompanyProfile <alanood94895@gmail.com>
2026-04-26 09:26:41 +0400

    Task1:EmployeeStackDemo

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -4,6 +4,7 @@ public class EmployeeStackDemo {                     // Create the main class
     public static void main (String[] args){
         // Create the Stack
         Stack <String> employeeAction = new Stack<>();
+        // Add 5 employee action
         employeeAction.push("employee Ali logged in");
         employeeAction.push("employee Noof updated client records");
         employeeAction.push("employee Noor generated report");
