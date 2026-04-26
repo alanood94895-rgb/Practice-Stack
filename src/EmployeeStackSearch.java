4c64a5691b2a3514437c5ee6e07e43f824d8090c
CompanyProfile <alanood94895@gmail.com>
2026-04-26 09:31:50 +0400

    Task3:EmployeeStackSearch

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -1,2 +1,16 @@
+import java.util.Stack;
+
 public class EmployeeStackSearch {
+    public static void main (String[] args){
+        Stack <String> employeeAction = new Stack<>();
+
+        employeeAction.push();
+        employeeAction.push();
+        employeeAction.push();
+        employeeAction.push();
+        employeeAction.push();
+        employeeAction.push();
+
+
+    }
 }
