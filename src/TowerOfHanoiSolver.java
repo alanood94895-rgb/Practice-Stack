9ae2fe397290543752247f40425ceac42b4125b5
CompanyProfile <alanood94895@gmail.com>
2026-04-26 14:50:25 +0400

    Task11:TowerOfHanoiSolver

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -9,5 +9,8 @@ public class TowerOfHanoiSolver {
         static Stack<Integer> pegB = new Stack<>();
         static Stack<Integer> pegC = new Stack<>();
 
-    }
+    // Counter for moves
+    static int moveCount = 0;
+
+}
 }
