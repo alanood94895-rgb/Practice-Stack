f03b1f0d44551be4673c3ba159dcb01cee39c259
CompanyProfile <alanood94895@gmail.com>
2026-04-26 12:26:48 +0400

    Task8: PostfixEvaluator

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -1,5 +1,12 @@
 public class PostfixEvaluator {
     public static void main (String[] args){
 
+        //Create 6 String element
+        String s1="1 2 +";
+        String s2="3 4 -";
+        String s3="5 7 *";
+        String s4="10 1 %";
+        String s5="2 9 /";
+        String s6="7 8 + *";
     }
 }
