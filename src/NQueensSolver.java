a847e6c8d69a38f923a2949933261e197bf4a172
CompanyProfile <alanood94895@gmail.com>
2026-04-27 14:56:31 +0400

    Task20:NQueensSolver

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -43,3 +43,9 @@ public static boolean isSafe(Position pos, Stack<Position> queens) {
 
     return true;
 }
+// 🔹 ITERATIVE STACK SOLUTION
+public static int solveNQueens(int n) {
+
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2 || n == 3) return 0;
