9ce456ac9bb2014eebf8ea2a44aef8a9bf1b5259
CompanyProfile <alanood94895@gmail.com>
2026-04-27 14:55:11 +0400

    Task20:NQueensSolver

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -1,2 +1,10 @@
 public class NQueensSolver {
+    // 🔹 POSITION CLASS
+    static class Position {
+        int row, col;
+
+        Position(int r, int c) {
+            row = r;
+            col = c;
+        }
 }
