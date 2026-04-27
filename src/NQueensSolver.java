2acfc662c56821d675ec55d2df83700338f66b45
CompanyProfile <alanood94895@gmail.com>
2026-04-27 14:55:25 +0400

    Task20:NQueensSolver

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -7,4 +7,13 @@ public class NQueensSolver {
             row = r;
             col = c;
         }
-}
+        public String toString() {
+            return "(" + row + "," + col + ")";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] testCases = {0, 1, 2, 3, 4, 5, 6, 8};
+
+    }
