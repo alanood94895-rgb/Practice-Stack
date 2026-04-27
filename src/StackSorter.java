b57a3aea85675d6bbbf9159daa05a0eb14da9229
CompanyProfile <alanood94895@gmail.com>
2026-04-27 10:53:05 +0400

    Task17:StackSorter

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -4,7 +4,7 @@ public class StackSorter {
 
     public static void main(String[] args) {
 
-        // 5 test stacks
+        // 5 stacks
         Stack<Integer> s1 = createStack(new int[]{3, 1, 4, 2, 5});
         Stack<Integer> s2 = createStack(new int[]{10, 9, 8, 7, 6});
         Stack<Integer> s3 = createStack(new int[]{1, 2, 3, 4, 5});
