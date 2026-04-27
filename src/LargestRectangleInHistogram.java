d02eecc1530f4731b9b7b3d8c175fdc5180f376d
CompanyProfile <alanood94895@gmail.com>
2026-04-27 14:27:27 +0400

    Task19:LargestRectangleInHistogram

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -153,4 +153,22 @@ public static int largestRectangleInMatrix(int[][] matrix) {
 
     int cols = matrix[0].length;
     int[] heights = new int[cols];
+    int maxArea = 0;
+
+    for (int[] row : matrix) {
+
+        for (int j = 0; j < cols; j++) {
+            if (row[j] == 1) {
+                heights[j] += 1;
+            } else {
+                heights[j] = 0;
+            }
+        }
+
+        maxArea = Math.max(maxArea, findLargestRectangle(heights));
+    }
+
+    return maxArea;
+}
+}
 
