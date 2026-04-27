7f54107548488b69137487785af07a5a11c25ae5
CompanyProfile <alanood94895@gmail.com>
2026-04-27 15:56:58 +0400

    Task21:StockSpanCalculator

--- src/StockSpanCalculator.java
+++ src/StockSpanCalculator.java
@@ -38,3 +38,17 @@ public static int[] calculateSpan(int[] prices) {
     Stack<Integer> stack = new Stack<>();
 
     for (int i = 0; i < n; i++) {
+        while (!stack.isEmpty() && prices[stack.peek()] <= prices[i]) {
+            stack.pop();
+        }
+
+        if (stack.isEmpty()) span[i] = i + 1;
+        else span[i] = i - stack.peek();
+
+        stack.push(i);
+    }
+
+    return span;
+}
+
+// 🔹 BRUTE FORCE O(n²)
