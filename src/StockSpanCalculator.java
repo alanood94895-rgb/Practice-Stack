7a56970dafdb2e507911944822a96474c312d13e
CompanyProfile <alanood94895@gmail.com>
2026-04-27 15:56:18 +0400

    Task21:StockSpanCalculator

--- src/StockSpanCalculator.java
+++ src/StockSpanCalculator.java
@@ -1,2 +1,13 @@
 public class StockSpanCalculator {
-}
+    public static void main(String[] args) {
+
+        int[][] testCases = {
+                {100, 80, 60, 70, 60, 75, 85},
+                {10, 20, 30, 40, 50},
+                {50, 40, 30, 20, 10},
+                {30},
+                {},
+                {20, 20, 20, 20}
+        };
+
+    }
