5c2337b11c7e7339f6f32f7da76a0db43a49afe6
CompanyProfile <alanood94895@gmail.com>
2026-04-27 14:25:32 +0400

    Task19:LargestRectangleInHistogram

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -1,2 +1,17 @@
 public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+
+        int[][] testCases = {
+                {2,1,5,6,2,3},
+                {2,4},
+                {6,2,5,4,5,1,6},
+                {1,1,1,1},
+                {5,4,3,2,1},
+                {1},
+                {},
+                {2,3,4,5,6}
+        };
+
+        for (int[] heights : testCases) {
+            System.out.println("\n==============================");
 }
