2196e6156326fcbb0944703ddcf7464fc2a124d9
CompanyProfile <alanood94895@gmail.com>
2026-04-27 09:50:21 +0400

    Task15: QueueUsingTwoStacks

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -10,6 +10,13 @@ public class QueueUsingTwoStacks {
             QueueUsingTwoStacks<Integer> queue = new QueueUsingTwoStacks<>();
 
             System.out.println("Queue Operations:");
-
+            queue.enqueue(10);
+            queue.enqueue(20);
+            queue.enqueue(30);
+            queue.peek();
+            queue.dequeue();
+            queue.enqueue(40);
+            queue.enqueue(50);
+            queue.dequeue()
         }
 }
