2e37fda278bd389fecd2e964f20e87ca66a0413c
CompanyProfile <alanood94895@gmail.com>
2026-04-27 10:12:15 +0400

    Task16: MinStack

--- src/MinStack.java
+++ src/MinStack.java
@@ -3,6 +3,7 @@ import java.util.Stack;
 public class MinStack {
     public static void main (String[] args){
         Stack<Integer> mainStack = new Stack<>();
+        Stack<Integer> minStack = new Stack<>();
 
     }
 }
